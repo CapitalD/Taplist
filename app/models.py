8f9749b6536b6dd29c8a0b05116fb68fa98c60f8
Duncan Thurston <duncan@capitald.co.nz>
2017-03-30 19:12:40 +1300

    Completed basic relationships and updated location template

--- app/models.py
+++ app/models.py
@@ -4,6 +4,9 @@ class Tap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     label = db.Column(db.String(255))
     location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    beer_id = db.Column(db.Integer, db.ForeignKey('beer.id'))
+    beer = db.relationship('Beer', backref='beer')
+
 
     def __repr__(self):
         return '<Tap %r>' % (self.label)
@@ -16,3 +19,25 @@ class Location(db.Model):
 
     def __repr__(self):
         return '<Location %r>' % (self.name)
+
+class Beer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    style = db.Column(db.String(255))
+    abv = db.Column(db.Float)
+    colour = db.Column(db.Integer)
+    brewery_id = db.Column(db.Integer, db.ForeignKey('brewery.id'))
+    brewery = db.relationship('Brewery', backref='brewery')
+    taps = db.relationship('Tap', backref='taps', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Beer %r>' % (self.name)
+
+class Brewery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    address = db.Column(db.String(255))
+    beers = db.relationship('Beer', backref='beers', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Brewery %r>' % (self.name)
