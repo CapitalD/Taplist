f816dfdebe444d536d4cd935ca29e00c1643ed87
Duncan Thurston <duncan@capitald.co.nz>
2017-03-31 13:20:28 +1300

    Added ability to change location, and added location dropdown in header

--- app/views.py
+++ app/views.py
@@ -4,11 +4,16 @@ from models import Location, Tap
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    all_locations = Location.query.all()
+    return render_template("index.html",
+                            all_locations=all_locations)
 
 @app.route('/location/<id>', methods=['GET'])
 def view_location(id):
     location = Location.query.get_or_404(id)
+    all_locations = Location.query.all()
+
     return render_template('view_location.html',
                             title=location.name,
-                            location=location)
+                            location=location,
+                            all_locations=all_locations)
